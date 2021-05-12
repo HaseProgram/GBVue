062d07ca81fe8a09d0fb6a08d17a97fecd46b23c
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2021-05-12 11:07:27 +0300

    lesson2; added code scrcst

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,74 @@
 <template>
   <div class="hello">
-    <input v-model.number="operand1" />
-    <input v-model.number="operand2" />
+    fib (<input v-model.number="operand1" />)
+    fib (<input v-model.number="operand2" />)
     = {{ result }}
     <div>
-      <button @click="calculate">+</button>
-      <button @click="calculate">-</button>
-      <button @click="calculate">/</button>
-      <button @click="calculate">*</button>
+      <button
+        v-for="op in operations"
+        :key="op"
+      @click="calculate(op)"
+      >{{ op }}</button>
+    </div>
+
+    <div class="error" v-if="error">
+      {{ error }}
+    </div>
+
+    <div v-if="result < 0">
+      Получилось отрицательное число
+    </div>
+    <div v-else-if="result <= 1000">
+      Результат леждит в пределах 0..1000
+    </div>
+    <div v-else-if="result <= 100000">
+      Результат леждит в пределах 1000..100000
+    </div>
+    <div v-else>
+      Результат слишком большой!
+    </div>
+    <div class="logs">
+      <div v-for="(log, id) in logs" :key="id">{{ log }}</div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
   data: () => ({
     operand1: 0,
     operand2: 0,
-    result: 0
+    result: 0,
+    error: '',
+    operations: ['+', '-', '/', '*'],
+    logs: {}
   }),
   props: {
   },
   methods: {
-    calculate (operation) {
+    fib (n) {
+      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2)
+    },
+    calculate (op) {
+      const operand1 = this.fib1
+      const operand2 = this.fib2
+      Vue.set(this.logs, Date.now(), `${operand1}${op}${operand2}=?`)
 
+      if (op === '/' && operand2 === 0) {
+        this.error = 'Division by zero!'
+        return
+      }
+
+      event.preventDefault()
+
+      const calcOperations = {
+        '+': () => operand1 + operand2,
+        '-': () => operand1 - operand2,
+        '/': () => operand1 / operand2,
+        '*': () => operand1 * operand2
+      }
+      this.result = calcOperations[op]()
     },
     sum () {
       this.result = this.operand1 + this.operand2
@@ -39,7 +84,12 @@ export default {
     }
   },
   computed: {
-
+    fib1 () {
+      return this.fib(this.operand1)
+    },
+    fib2 () {
+      return this.fib(this.operand2)
+    }
   },
   watch: {
 
@@ -62,5 +112,9 @@ li {
 }
 a {
   color: #42b983;
+}
+.error {
+  padding: 20px;
+  border: 1px solid red;
 }
 </style>
