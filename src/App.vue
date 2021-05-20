65c15fc1dc4106ab74c022ad27561b988cc2133d
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2021-05-20 20:56:04 +0300

    lesson5hash; hashedurl

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,45 @@
   <div id="app">
     <header class="header">
       My personal costs
+      <a href="#dashboard">Dashboard</a>
+      <a href="#about">About</a>
+      <a href="#404">404</a>
     </header>
     <main>
-      <PaymentForm />
-      <PaymentsList />
+      <PageDashboard v-if="page === 'dashboard'" />
+      <PageAbout  v-if="page === 'about'" />
+      <Page404  v-if="page === '404'" />
     </main>
   </div>
 </template>
 
 <script>
-import PaymentsList from './components/PaymentsList'
-import PaymentForm from './components/PaymentForm'
-
-import { mapActions } from 'vuex'
+import PageDashboard from './page/PageDashboard'
+import PageAbout from './page/PageAbout'
+import Page404 from './page/Page404'
 
 export default {
   name: 'App',
   components: {
-    PaymentsList,
-    PaymentForm
+    PageDashboard,
+    PageAbout,
+    Page404
   },
   data () {
     return {
+      page: 'dashboard'
     }
   },
   methods: {
-    // ...mapActions([
-    //   'fetchData'
-    // ]),
-    ...mapActions({
-      fetchListData: 'fetchData'
-    }),
-    onDataAdded (data) {
-      this.paymentsList.push(data)
+    setPage () {
+      this.page = location.hash.slice(1)
     }
   },
   mounted () {
-    this.fetchListData()
+    this.setPage()
+    window.addEventListener('hashchange', () => {
+      this.setPage()
+    })
   }
 }
 </script>
---
+++ src/page/PageDashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <PaymentForm />
+    <PaymentsList />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+import PaymentForm from '../components/PaymentForm'
+import PaymentsList from '../components/PaymentsList'
+
+export default {
+  components: {
+    PaymentForm,
+    PaymentsList
+  },
+  methods: {
+    ...mapActions({
+      fetchListData: 'fetchData'
+    })
+  },
+  mounted () {
+    this.fetchListData()
+  }
+}
+</script>
+
+<style>
+
+</style>
